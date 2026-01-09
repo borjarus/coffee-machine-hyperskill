0496b095d53cb4d89cd97905efbc22c7099d7769
Mirosław Puczyński <miroslaw.puczynski@currenda.pl>
2026-01-09 10:54:23 +0100

    stage 2

--- Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -1,5 +1,24 @@
 package machine
 
+data class Ingredients(val water: Int, val milk: Int, val beans: Int)
+
 fun main() {
-    println("Hello, world!")
+    val cups = readln()
+        .toIntOrNull()
+        ?.takeIf { it > 0 }
+        ?: run {
+            println("Invalid input")
+            return
+        }
+
+    val ingredients = listOf(200, 50, 15)
+        .map { it * cups }
+        .let { Ingredients(it[0], it[1], it[2]) }
+
+    """
+For ${'$'}cups cups of coffee you will need:
+${ingredients.water} ml of water
+${ingredients.milk} ml of milk
+${ingredients.beans} g of coffee beans
+    """.trimIndent().also (::println )
 }
