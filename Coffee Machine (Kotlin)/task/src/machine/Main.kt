0125fe88b0fa1e500d3fff256208b15a8bbfbce0
Mirosław Puczyński <miroslaw.puczynski@currenda.pl>
2026-01-09 11:21:05 +0100

    stage 3

--- Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -1,24 +1,30 @@
 package machine
 
-data class Ingredients(val water: Int, val milk: Int, val beans: Int)
+data class Supplies(val water: Int, val milk: Int, val beans: Int)
+
+fun readAmount(prompt: String): Int {
+    print("$prompt ")
+    return readln().toInt()
+}
 
 fun main() {
-    val cups = readln()
-        .toIntOrNull()
-        ?.takeIf { it > 0 }
-        ?: run {
-            println("Invalid input")
-            return
-        }
+    val supplies = Supplies(
+        readAmount("Write how many ml of water the coffee machine has:"),
+        readAmount("Write how many ml of milk the coffee machine has:"),
+        readAmount("Write how many grams of coffee beans the coffee machine has:")
+    )
 
-    val ingredients = listOf(200, 50, 15)
-        .map { it * cups }
-        .let { Ingredients(it[0], it[1], it[2]) }
+    val cups = readAmount("Write how many cups of coffee you will need:")
 
-    """
-For ${'$'}cups cups of coffee you will need:
-${ingredients.water} ml of water
-${ingredients.milk} ml of milk
-${ingredients.beans} g of coffee beans
-    """.trimIndent().also (::println )
+    val maxCups = listOf(
+        supplies.water / 200,
+        supplies.milk / 50,
+        supplies.beans / 15
+    ).min()
+
+    when {
+        cups < maxCups -> "Yes, I can make that amount of coffee (and even ${maxCups - cups} more than that)"
+        cups == maxCups -> "Yes, I can make that amount of coffee"
+        else -> "No, I can make only $maxCups cups of coffee"
+    }.also(::println)
 }
