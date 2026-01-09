db9a318e72853ece0954f74d980b07e03ab57a54
Mirosław Puczyński <miroslaw.puczynski@currenda.pl>
2026-01-09 13:49:09 +0100

    stage 4

--- Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -1,30 +1,95 @@
 package machine
 
-data class Supplies(val water: Int, val milk: Int, val beans: Int)
+data class CoffeeMachine(
+    val water: Int = 400,
+    val milk: Int = 540,
+    val beans: Int = 120,
+    val cups: Int = 9,
+    val money: Int = 550
+)
 
-fun readAmount(prompt: String): Int {
-    print("$prompt ")
-    return readln().toInt()
+data class Coffee(val water: Int, val milk: Int, val beans: Int, val price: Int)
+
+val coffees = mapOf(
+    1 to Coffee(250, 0, 16, 4), // espresso
+    2 to Coffee(350, 75, 20, 7), // latte
+    3 to Coffee(200, 100, 12, 6), // cappuccino
+)
+
+fun printState(machine: CoffeeMachine) {
+    println("${machine.water} ml of water")
+    println("${machine.milk} ml of milk")
+    println("${machine.beans} g of coffee beans")
+    println("${machine.cups} disposable cups")
+    println("\$${machine.money} of money")
+    println()
+}
+
+fun canMakeCoffee(machine: CoffeeMachine, coffee: Coffee): Boolean =
+    machine.water >= coffee.water &&
+    machine.milk >= coffee.milk &&
+    machine.beans >= coffee.beans &&
+    machine.cups > 0
+
+fun makeCoffee(machine: CoffeeMachine, coffee: Coffee): CoffeeMachine =
+    CoffeeMachine(
+        machine.water - coffee.water,
+        machine.milk - coffee.milk,
+        machine.beans - coffee.beans,
+        machine.cups - 1,
+        machine.money + coffee.price
+    )
+
+
+fun buyCoffee(machine: CoffeeMachine): CoffeeMachine {
+    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+    val choice = readln().trim().toInt()
+
+    val coffee = coffees[choice]
+    return if (coffee != null && canMakeCoffee(machine, coffee)) {
+        makeCoffee(machine, coffee)
+    } else {
+        machine
+    }
+}
+
+fun fillSupplies(machine: CoffeeMachine): CoffeeMachine {
+    print("Write how many ml of water you want to add: ")
+    val water = readln().trim().toInt()
+    print("Write how many ml of milk you want to add: ")
+    val milk = readln().trim().toInt()
+    print("Write how many grams of coffee beans you want to add: ")
+    val beans = readln().trim().toInt()
+    print("Write how many disposable cups you want to add: ")
+    val cups = readln().trim().toInt()
+
+    return CoffeeMachine(
+        machine.water + water,
+        machine.milk + milk,
+        machine.beans + beans,
+        machine.cups + cups,
+        machine.money
+    )
+}
+
+fun takeMoney(machine: CoffeeMachine): CoffeeMachine {
+    println("I gave you \$${machine.money}")
+    return CoffeeMachine(machine.water, machine.milk, machine.beans, machine.cups, 0)
 }
 
 fun main() {
-    val supplies = Supplies(
-        readAmount("Write how many ml of water the coffee machine has:"),
-        readAmount("Write how many ml of milk the coffee machine has:"),
-        readAmount("Write how many grams of coffee beans the coffee machine has:")
-    )
+    var machine = CoffeeMachine()
 
-    val cups = readAmount("Write how many cups of coffee you will need:")
+    println("The coffee machine has:")
+    printState(machine)
 
-    val maxCups = listOf(
-        supplies.water / 200,
-        supplies.milk / 50,
-        supplies.beans / 15
-    ).min()
+    println("Write action (buy, fill, take): ")
+    when (readln().trim()) {
+        "buy" -> machine = buyCoffee(machine)
+        "fill" -> machine = fillSupplies(machine)
+        "take" -> machine = takeMoney(machine)
+    }
 
-    when {
-        cups < maxCups -> "Yes, I can make that amount of coffee (and even ${maxCups - cups} more than that)"
-        cups == maxCups -> "Yes, I can make that amount of coffee"
-        else -> "No, I can make only $maxCups cups of coffee"
-    }.also(::println)
+    println("\nThe coffee machine has:")
+    printState(machine)
 }
